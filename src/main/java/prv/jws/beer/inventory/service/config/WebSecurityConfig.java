e5974c79db15dd48373ccb6d212d8b5bc003f1b7
Jurek <jurek.szymanski@gmail.com>
2020-10-06 11:12:39 +0200

    added starter-security to pom, added user and password to use for accessing and set up basic authentication in http configuration. Closes #7

---
+++ src/main/java/prv/jws/beer/inventory/service/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+package prv.jws.beer.inventory.service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Jerzy Szymanski on 06.10.2020 at 11:07
+ */
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable()
+        .authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .httpBasic();
+    }
+}
